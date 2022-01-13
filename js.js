e46400abd9c233de51173a1a29096be6e0ba5c5b
negrelias <negrelias1992@gmail.com>
2022-01-13 10:02:54 +0000

    guia de estudos

--- js.js
+++ js.js
@@ -1,5 +1,13 @@
-let apresent = document.getElementById("titulo");
+const { clear } = require('console');
+const { constants } = require('perf_hooks');
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var [a,b,c] = input.split('\n').map(Number);
+var funcionario =( b * c) ;
+var soma = funcionario;
+var result= soma.toFixed(2)
 
 
+process.stdout.write(`NUMBER = ${a}\n`);
+process.stdout.write(`SALARY = U$ ${result}\n`);
 
- alert(`meu primeiro dom com ${apresent}`)
